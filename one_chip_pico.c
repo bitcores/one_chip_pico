4b277b8bf95cceaf0d6b6a708208c4bd805efa34
bitcores <bitcores@gmail.com>
2023-10-13 20:48:39 +0900

    Add files via upload

--- one_chip_pico.c
+++ one_chip_pico.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "pico/multicore.h"
 #include "hardware/gpio.h"
+//#include "hardware/vreg.h"
 
 #include "one_chip_in.pio.h"
 #include "one_chip_out.pio.h"
 #include "prg_data.h"
-#include "prg_ram.h"
+//#include "prg_ram.h"
+
+// set up an empty 8kB for prg_ram. comment if using prg_ram.h
+uint8_t prg_ram[8192];
+
+int main(void){
+    //vreg_set_voltage(VREG_VOLTAGE_1_15);
+    set_sys_clock_khz(270000, true);
+
+    // if clock works and pico will be ready, turn on LED
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1);
 
 
-void core1_main() {
-    
     // setup pio program for handling input
     PIO pioin = pio0;
     uint offsetin = pio_add_program(pioin, &one_chip_in_program);
@@ -23,16 +33,8 @@ void core1_main() {
     uint smout = pio_claim_unused_sm(pioout, true);
     one_chip_out_program_init(pioout, smout, offsetout, 15);
 
-    //uint32_t bugger[105];
-    //int b = 0;
-
     while(1) {
         uint32_t pointer = pio_sm_get_blocking(pioin, smin);
-
-        /*if (b < 100) {
-            bugger[b] = pointer;
-            b++;
-        }*/
         
         if (!(pointer & 67108864)) {        // check if prg rom space
             if (pointer & 134217728) {       // is read
@@ -52,63 +54,8 @@ void core1_main() {
             }
         }
 
-
-        /*if (b == 100) {
-            for (int c = 0; c < 100; c++) {
-                int prg = 0;
-
-                if (!(bugger[c] & 67108864)) {
-                    printf("PRG-ROM ");
-                    printf("%X ", ((bugger[c] & 32767) + 32768));
-                    prg = 1;
-                } else if ((bugger[c] & 24576) == 24576) {
-                    printf("PRG-RAM ");
-                    printf("%X ", (bugger[c] & 32767));
-                    prg = 2;
-                } else {
-                    printf("ADDR    ");
-                    printf("%X ", (bugger[c] & 32767));
-                }
-
-
-                uint8_t dbugdata = bugger[c] >> 15;
-
-                if (bugger[c] & 134217728) {
-                    printf("read  ");
-                    if (prg == 1)
-                        dbugdata = prg_data[bugger[c] & 32767];
-                    else if (prg == 2)
-                        dbugdata = prg_ram[bugger[c] & 8191];
-                } else {
-                    printf("write ");
-                }
-
-                printf("%X \n", dbugdata);
-
-            }
-
-            b = 1000;
-        }*/
+        pio_sm_clear_fifos(pioin, smin);
         
-        
-    }
-}
-
-
-int main(void){
-    set_sys_clock_khz(270000, true);
-    stdio_init_all();
-
-    // if clock works and pico will be ready, turn on LED
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
-
-    multicore_launch_core1(core1_main);
-
-    while(1) {
-        tight_loop_contents();
     }
 
     return 0;
